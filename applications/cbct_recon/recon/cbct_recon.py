8160c39c1eb5d5f5063501dca6a0052787c47a18
Zhijin Li <zhijinl@nvidia.com>
2024-08-02 21:59:14 +0200

    Fix license headers.

--- applications/cbct_recon/recon/cbct_recon.py
+++ applications/cbct_recon/recon/cbct_recon.py
@@ -1,14 +1,17 @@
-#! /usr/bin/env python3
-## ---------------------------------------------------------------------------
-##
-## File: app.py<cbct_recon> for CBCT Recon
-##
-## Created by Zhijin Li
-## E-mail:   <zhijinl@nvidia.com>
-##
-## Started on  Mon May 13 18:04:23 2024 Zhijin Li
-## Last update Fri Aug  2 21:11:38 2024 Zhijin Li
-## ---------------------------------------------------------------------------
+# SPDX-FileCopyrightText: Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 import os
